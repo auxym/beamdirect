eb9094b4356e7a4dd862025df46e1d2f2743218e
Francis Therien <frtherien@gmail.com>
2019-11-02 08:36:09 -0400

    Add symmetric check func

--- tests/utils_test.nim
+++ tests/utils_test.nim
@@ -1,12 +1,21 @@
 import arraymancer
 
 func triutosym*[T: SomeNumber](a: Tensor[T]) : Tensor[T] =
-    ## Createe symmetric matrix from upper triangular part
+    ## Create symmetric matrix from upper triangular part
     result = zeros_like(a)
     let N = a.shape[0]
-
-    for i in countup(0, N-1):
-        for j in countup(i, N-1):
+    for i in 0..<N:
+        for j in i..<N:
             result[i, j] = a[i, j]
             if i != j:
                 result[j, i] = a[i, j]
+
+func isSymmetric*[T: SomeNumber](a: Tensor[T]): bool =
+    let N = a.shape[0]
+    for i in 0..<N:
+        for j in 0..<N:
+            if i == j:
+                continue
+            elif a[i, j] != a[j, i]:
+                return false
+    return true
